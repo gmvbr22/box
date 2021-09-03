2f0e7ddda4080138b6523437a41b435128ee576a
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-03 07:46:29 -0400

    Create ReadToken

---
+++ pkg/ast/tree.go
@@ -0,0 +1,57 @@
+package ast
+
+import "fmt"
+
+const (
+	Keyword    = "Keyword"
+	Identifier = "Identifier"
+	Operator   = "Operator"
+	Separator  = "Separator"
+)
+
+type Token struct {
+	Type   string
+	Value  string
+	Line   int
+	Column int
+}
+
+type SingleRule struct {
+	Type  string
+	Value string
+	Child interface{}
+}
+
+var program = &SingleRule{
+	Type:  Keyword,
+	Value: "package",
+	Child: &SingleRule{
+		Type: Identifier,
+		Child: &SingleRule{
+			Type: Identifier,
+		},
+	},
+}
+
+var read interface{} = nil
+
+func ReadToken(token *Token) {
+	if read == nil {
+		read = program
+	}
+	if rule, isRule := read.(SingleRule); isRule {
+		if token.Type != rule.Type {
+			panic(fmt.Sprintf("Expected %v but got %v [Line: %d, Col: %d]",
+				rule.Type, token.Type, token.Line, token.Column,
+			))
+		}
+		if rule.Value != "" && rule.Value != token.Value {
+			panic(fmt.Sprintf("Expected %v but got %v [Line: %d, Col: %d]",
+				rule.Value, token.Value, token.Line, token.Column,
+			))
+		}
+		read = rule.Child
+		fmt.Printf("Line: %d, Offset: %d, Type: %s, Value: %s\n", token.Line, token.Column, token.Type, token.Value)
+		return
+	}
+}
